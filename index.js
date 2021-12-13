32d7599c1f01d277d265822cbc2b97b57c22cf0b
fbclh <fbclh@protonmail.com>
2021-12-12 23:07:35 -0300

    Attempt to add mobile navbar menu

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 // Mobile menu (Dynamic HTML)
+const burgerIcon = document.querySelector('.nav__icon-open');
+const mobileMenu = document.querySelector('.nav__mobile-menu');
+const closeIcon = document.querySelector('.nav__icon-close');
+const menuLinks = document.querySelectorAll('.nav__mobile');
+
+burgerIcon.addEventListener('click', () => {
+  mobileMenu.classList.toggle('test');
+});
+
+closeIcon.addEventListener('click', () => {
+  mobileMenu.classList.toggle('test');
+});
+
+menuLinks.forEach((el) => {
+  el.addEventListener('click', () => {
+    mobileMenu.classList.toggle('test');
+  });
+});
 
 // Popup window (JS objects)
 
