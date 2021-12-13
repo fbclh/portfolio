7ef5052e5975b4fc860c79654a4424c010dc42a3
Zeeshan Haider <zhadier39@gmail.com>
2021-12-13 22:48:59 +0500

    Add variables using queryselector in JS

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Mobile menu (Dynamic HTML)
+// Mobile menu (Dynamic HTML)
+const openIcon = document.querySelector(".nav__icon-open");
+const mobileMenu = document.querySelector(".nav__mobile-menu");
+const closeIcon = document.querySelector(".nav__icon-close");
+const menuLinks = document.querySelectorAll(".nav__mobile");
 
 // Popup window (JS objects)
 
