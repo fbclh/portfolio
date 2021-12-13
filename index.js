3155ee3904a6a12a2082f42f2fc16736f8a1f14b
fbclh <fbclh@protonmail.com>
2021-12-13 09:04:22 -0300

    Remove nav mobile menu test

--- index.js
+++ index.js
@@ -6,24 +6,3 @@
 
 // Preserve data in the browser (Local storage)
 
-//////////////////////////////////////////////////////////////////////////////
-
-// 
-const openIcon = document.querySelector('.nav__icon-open');
-const mobileMenu = document.querySelector('.nav__mobile-menu');
-const closeIcon = document.querySelector('.nav__icon-close');
-const menuLinks = document.querySelectorAll('.nav__mobile');
-
-closeIcon.addEventListener('click', () => {
-  mobileMenu.style.left = '-100%';
-});
-
-openIcon.addEventListener('click', () => {
-  mobileMenu.style.left = '0';
-});
-
-menuLinks.forEach((item) => {
-  item.addEventListener('click', () => {
-    mobileMenu.style.left = '-100%';
-  });
-});
