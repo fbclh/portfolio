7c5a0000ed895b3421b62c419ab83f7250076931
fbclh <fbclh@protonmail.com>
2021-12-12 23:58:20 -0300

    Fix nav mobile menu

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 // Mobile menu (Dynamic HTML)
-const burgerIcon = document.querySelector('.nav__icon-open');
+// const burgerIcon = document.querySelector('.nav__icon-open');
+const openIcon = document.querySelector('.nav__icon-open');
 const mobileMenu = document.querySelector('.nav__mobile-menu');
 const closeIcon = document.querySelector('.nav__icon-close');
 const menuLinks = document.querySelectorAll('.nav__mobile');
 
-burgerIcon.addEventListener('click', () => {
-  mobileMenu.classList.toggle('test');
+closeIcon.addEventListener('click', () => {
+  mobileMenu.style.left = '-110%';
 });
 
-closeIcon.addEventListener('click', () => {
-  mobileMenu.classList.toggle('test');
+openIcon.addEventListener('click', () => {
+  mobileMenu.style.left = '0';
 });
+
+// burgerIcon.addEventListener('click', () => {
+//   mobileMenu.classList.toggle('test');
+// });
+
+// closeIcon.addEventListener('click', () => {
+//   mobileMenu.classList.toggle('test');
+// });
 
 menuLinks.forEach((el) => {
   el.addEventListener('click', () => {
     mobileMenu.classList.toggle('test');
   });
 });
+
+//
+
+// const mobileMenu = document.getElementById('menu');
+// const closeIcon = document.getElementById('close-menu');
+// const openIcon = document.getElementById('hamburger-icon');
+
+// closeIcon.addEventListener('click', () => {
+//   mobileMenu.style.left = '-110%';
+// });
+
+// openIcon.addEventListener('click', () => {
+//   mobileMenu.style.left = '0';
+// });
+
+// document.querySelectorAll('.classList').forEach((item) => {
+//   item.addEventListener('click', () => {
+//     mobileMenu.style.left = '-110%';
+//   });
+// });
+
+//
 
 // Popup window (JS objects)
 
