46cff5544b8f7577813309e9370007bbd05bb53e
fbclh <fbclh@protonmail.com>
2021-12-13 17:01:56 -0300

    Fix JS and CSS linter errors

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Mobile menu (Dynamic HTML)
 
-const openIcon = document.querySelector(".nav__icon-open");
-const mobileMenu = document.querySelector(".nav__mobile-menu");
-const closeIcon = document.querySelector(".nav__icon-close");
-const menuLinks = document.querySelectorAll(".nav__mobile");
+const openIcon = document.querySelector('.nav__icon-open');
+const mobileMenu = document.querySelector('.nav__mobile-menu');
+const closeIcon = document.querySelector('.nav__icon-close');
+const menuLinks = document.querySelectorAll('.nav__mobile');
 
 closeIcon.addEventListener('click', () => {
   mobileMenu.style.left = '-100%';
@@ -20,8 +20,6 @@ menuLinks.forEach((el) => {
 });
 
 // Popup window (JS objects)
-
 // Validate contact form (Client side validation)
-
 // Preserve data in the browser (Local storage)
 
