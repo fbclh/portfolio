efaa6e16e1a156e7b7e3183894bccc0fe885e65f
fbclh <fbclh@protonmail.com>
2021-12-13 14:56:04 -0300

    Finish the main JS and parts of the CSS

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 // Mobile menu (Dynamic HTML)
-// Mobile menu (Dynamic HTML)
 const openIcon = document.querySelector(".nav__icon-open");
 const mobileMenu = document.querySelector(".nav__mobile-menu");
 const closeIcon = document.querySelector(".nav__icon-close");
 const menuLinks = document.querySelectorAll(".nav__mobile");
+
+closeIcon.addEventListener('click', () => {
+  mobileMenu.style.left = '-100%';
+});
+
+openIcon.addEventListener('click', () => {
+  mobileMenu.style.left = '0';
+});
+
+menuLinks.forEach((el) => {
+  el.addEventListener('click', () => {
+    mobileMenu.style.left = '-100%';
+  });
+});
 
 // Popup window (JS objects)
 
